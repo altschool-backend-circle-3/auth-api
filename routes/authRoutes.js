7363b58120a875e2b04b6423a7378b8e8a9a0f2c
mariam-adepoju <marrizon.am@gmail.com>
2026-07-26 15:04:46 +0100

    fix: signup controller reference error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ import authMiddleware from '../middleware/authMiddleware.js';
 import { signup, login, changePassword } from '../controllers/authController.js';
 
 const router = Router();
-router.route("/signup").post(signupUser);
+router.route("/signup").post(signup);
 router.route("/login").post(login);
 router.route("/change-password").patch(authMiddleware, changePassword);
 
